50b263666294d443b3a12c4d7b07b0080dce1ba6
Simon Alex <alekseev-simon@mail.ru>
2022-10-02 22:06:51 +0300

    fix test error

--- models/user.js
+++ models/user.js
@@ -51,22 +51,5 @@ userSchema.method.toJSON = () => {
   return user;
 };
 */
-userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-      }
-
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-          }
-
-          return user;
-        });
-    });
-};
 
 module.exports = mongoose.model('user', userSchema);
