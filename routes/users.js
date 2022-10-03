dc41f123c1ca1e26dd6083c531191dc0996caab1
Simon Alex <alekseev-simon@mail.ru>
2022-10-03 12:57:40 +0300

    fix test error

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,11 @@ const {
   updateUserInformation,
   updateUserAvatar,
 } = require('../controllers/users');
-const { validateUpdateUser, validateUpdateAvatar } = require('../middlewares/validator');
+const { validateUpdateUser, validateUpdateAvatar, validateUserId } = require('../middlewares/validator');
 
 userRoutes.get('/', getAllUsers);
 userRoutes.get('/me', getUser);
+userRoutes.get('/:userId', validateUserId, getUser);
 userRoutes.patch('/me', validateUpdateUser, updateUserInformation);
 userRoutes.patch('/me/avatar', validateUpdateAvatar, updateUserAvatar);
 
